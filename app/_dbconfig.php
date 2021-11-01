b0b2935f42afe3a9c56559849c22c9167167e734
SvoekoshinFA <53422625+SvoekoshinFA@users.noreply.github.com>
2021-11-01 12:59:21 +0400

    Update _dbconfig.php

--- app/_dbconfig.php
+++ app/_dbconfig.php
@@ -1,5 +1,5 @@
 <?php
     define('DB_HOST','mysql');
     define('DB_USER','root');
-    define('DB_PASS','example');
+    define('DB_PASS','change_to_your_DB_password');
     define('DB_NAME','');
